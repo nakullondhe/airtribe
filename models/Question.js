14955a2c8a018a47e72c86e83b2ff5556d32f912
Nakul Londh <nakul@Nakul-Londh.local>
2022-05-29 11:28:56 +0530

    Database Schema Added

---
+++ models/Question.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const QuestionSchema = new mongoose.Schema({
+    title: String,
+    link: {type: String, required: true, unique: true},
+    answers: Number,
+    votes: Number,
+    views: Number,
+    questionId: {type: String, required: true, unique: true},
+    referenceCount: {type: Number, default: 0},
+})
+
+module.exports = mongoose.model('Question', QuestionSchema)
