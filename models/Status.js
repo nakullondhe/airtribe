a9b14431ac323d6e0c8392bbf0b43551e616cbe0
Nakul Londh <nakul@Nakul-Londh.local>
2022-05-31 02:13:11 +0530

    updated Status schema

--- models/Status.js
+++ models/Status.js
@@ -4,7 +4,6 @@ const statusSchema = new mongoose.Schema({
     status: {type: String, enum: ['running', 'paused', 'stopped'], default: 'stopped'},
     recentLink: {type: String, default: ''},
     stop: {type: Boolean, default: false},
-    repeats: {type: Number, default: 0, max: 3}
 })
 
 module.exports = mongoose.model('Status', statusSchema);
